e0b57a11ef8d18c5f99e7b69d44eb27b2a0800d7
Alex Malik <oleksii.malyk [at] gmail.com>
2021-03-23 17:59:09 +0200

    Add unit tests for NumberedSquareFactory class

---
+++ Puzzle.UnitTests/NumberedSquareFactoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Puzzle.UnitTests
+{
+    public class NumberedSquareFactoryTests
+    {
+        [Test]
+        public void CreateByPosition_AsExpected_ReturnsNewNumberedSquare()
+        {
+            var squareFactory = new NumberedSquareFactory();
+
+            var square = squareFactory.CreateByPosition(0);
+            Assert.IsNotNull(square);
+            Assert.IsInstanceOf<NumberedSquare>(square);
+            
+            square = squareFactory.CreateByPosition(7);
+            Assert.IsNotNull(square);
+            Assert.IsInstanceOf<NumberedSquare>(square);
+            
+            square = squareFactory.CreateByPosition(13);
+            Assert.IsNotNull(square);
+            Assert.IsInstanceOf<NumberedSquare>(square);
+            
+            square = squareFactory.CreateByPosition(15);
+            Assert.IsNotNull(square);
+            Assert.IsInstanceOf<NumberedSquare>(square);
+        }
+
+        [Test]
+        public void CreateByPosition_PositionIsOutOfRange_ThrowsOutOfRangeException()
+        {
+            var squareFactory = new NumberedSquareFactory();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => squareFactory.CreateByPosition(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => squareFactory.CreateByPosition(-13));
+            Assert.Throws<ArgumentOutOfRangeException>(() => squareFactory.CreateByPosition(16));
+            Assert.Throws<ArgumentOutOfRangeException>(() => squareFactory.CreateByPosition(100));
+        }
+    }
+}

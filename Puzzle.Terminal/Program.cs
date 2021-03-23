9e37e075d748f44ac00c7f6632ddf552aa44098d
Alex Malik <oleksii.malyk [at] gmail.com>
2021-03-23 14:05:47 +0200

    Add missed files from previous commit
    Accidentally missed to add files that JetBrains Rider hide by "Unversioned Files" tab :)

---
+++ Puzzle.Terminal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Puzzle.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Game dependencies and parameters.
+            var factory = new NumberedSquareFactory();
+
+            var game = new Game(factory);
+
+            game.Start();
+
+            do
+            {
+                DisplayBoard(game._board);
+                
+                var input = Console.ReadKey();
+                if (input.Key == ConsoleKey.Q) break;
+                
+                if (input.Key == ConsoleKey.W)
+                    game.SlideUp();
+                else if (input.Key == ConsoleKey.S)
+                    game.SlideDown();
+                else if (input.Key == ConsoleKey.A)
+                    game.SlideLeft();
+                else if (input.Key == ConsoleKey.D)
+                    game.SlideRight();
+                else
+                    continue;
+            } while (true);
+        }
+
+        static void DisplayBoard(Square[] board)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var square = board[i * 4 + j] as NumberedSquare;
+                    Console.Write($"{square?.DisplayNumber.PadLeft(2) ?? "  "} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

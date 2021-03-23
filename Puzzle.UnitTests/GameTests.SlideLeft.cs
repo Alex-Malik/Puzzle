afb642d1f72d702ada06531531cd0519df1c05c2
Alex Malik <oleksii.malyk [at] gmail.com>
2021-03-23 16:52:04 +0200

    Add unit tests for SlideLeft method

---
+++ Puzzle.UnitTests/GameTests.SlideLeft.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Puzzle.UnitTests
+{
+    public partial class GameTests
+    {
+        [Test]
+        public void SlideLeft_GameNotStarted_ThrowsAnException()
+        {
+            var game = new Game(MockSquareFactory().Object);
+
+            Assert.Throws<Exception>(() => game.SlideLeft());
+        }
+        
+        [Test]
+        public void SlideLeft_HasSpaceToSlide_SlidesLeft()
+        {
+            var initialSequence = new[]
+            {
+                0,  1,  2,  3,
+                4,      6,  7,
+                8,  9,  10, 11,
+                12, 13, 14, 15, 5 // - the last index is the empty square position
+            };
+
+            var game = new Game(
+                MockSquareFactory().Object,
+                MockRandomizer(initialSequence).Object);
+            
+            game.Start();
+            game.SlideLeft();
+            
+            Assert.IsNull(game.Board.ToArray()[6]);
+            Assert.IsTrue(game.Board.Count(square => square != null) == 15);
+        }
+        
+        [Test]
+        public void SlideLeft_HasNoSpaceToSlide_DoesNothing()
+        {
+            var initialSequence = new[]
+            {
+                0,  1,  2,  3,
+                4,  5,  6, 
+                8,  9,  10, 11,
+                12, 13, 14, 15, 7 // - the last index is the empty square position
+            };
+
+            var game = new Game(
+                MockSquareFactory().Object,
+                MockRandomizer(initialSequence).Object);
+            
+            game.Start();
+            game.SlideLeft();
+            
+            Assert.IsNull(game.Board.ToArray()[7]);
+            Assert.IsTrue(game.Board.Count(square => square != null) == 15);
+        }
+        
+        [Test]
+        public void SlideLeft_LastSlideToWon_GameWon()
+        {
+            var initialSequence = new[]
+            {
+                0,  1,  2,  3,
+                4,  5,  6,  7,
+                8,  9,  10, 11,
+                12, 13, 14,     15 // - the last index is the empty square position
+            };
+
+            var game = new Game(
+                MockSquareFactory().Object,
+                MockRandomizer(initialSequence).Object);
+            
+            game.Start();
+            game.SlideRight();
+            game.SlideLeft();
+            
+            Assert.IsTrue(game.IsFinished);
+        }
+    }
+}

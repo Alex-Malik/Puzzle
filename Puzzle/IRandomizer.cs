e8149df862a88e7fe944d7e80a873e35a1ccb571
Alex Malik <oleksii.malyk [at] gmail.com>
2021-03-23 17:43:10 +0200

    Move randomizer's implementation out of it's interface file

--- Puzzle/IRandomizer.cs
+++ Puzzle/IRandomizer.cs
@@ -1,24 +1,7 @@
-﻿using System;
-using System.Linq;
-
-namespace Puzzle
+﻿namespace Puzzle
 {
     public interface IRandomizer
     {
         int[] GenerateRandomSequence(int length);
-    }
-
-    internal class DefaultRandomizer : IRandomizer
-    {
-        public int[] GenerateRandomSequence(int length)
-        {
-            if (length <= 0)
-                throw new ArgumentOutOfRangeException();
-            
-            var random = new Random();
-            var randomSequence = Enumerable.Range(0, length).OrderBy(_ => random.Next());
-
-            return randomSequence.ToArray();
-        }
     }
 }
